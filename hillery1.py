8dde29b51feee76f6f8703f524089026a1e7387e
Harry <harryshahai@gmail.com>
2019-05-23 10:19:01 -0700

    ghz and imports

--- hillery1.py
+++ hillery1.py
@@ -1,6 +1,12 @@
-from pyquil import Program
-from pyquil.gates import X, I, Y, Z
+from pyquil.quilatom import QubitPlaceholder
+from pyquil.quil import address_qubits
+from pyquil import Program, get_qc
+from pyquil.gates import X, I, Y, Z, H, CNOT, MEASURE
 from secret import SecretShare
+from pyquil.api import WavefunctionSimulator
+from helpers import ghz, measure_X
+
+N = 3
 
 class HillerySecretShare1(SecretShare):
     """The first secret sharing protocol from
@@ -14,5 +20,16 @@ class HillerySecretShare1(SecretShare):
     def recombine(self, shares):
         pass
 
+if __name__ == '__main__':
+    qubits = QubitPlaceholder.register(N)
+    x = ghz(qubits)
+    ro = x.declare('ro')
+    x += measure_X(qubits[0], ro, 0)
 
+    x = address_qubits(x)
+    qc = get_qc("5q-qvm")
+    print(qc.run(x))
+
+    wfn = WavefunctionSimulator().wavefunction(x)
+    print(wfn)
 
