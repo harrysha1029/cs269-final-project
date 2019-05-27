965c2fbf2034fb25ad54cf8d202bd99c17fd7166
Harry <harryshahai@gmail.com>
2019-05-26 23:24:51 -0700

    complete hillery1, start hillery2

--- consts.py
+++ consts.py
@@ -2,6 +2,22 @@ import numpy as np
 from pyquil.quil import DefGate
 
 num2direction = {0:'x', 1:'y'}
+VALID_DIRECTIONS = [
+        (('A', 'x'), ('B', 'x'), ('C', 'x')),
+        (('A', 'y'), ('B', 'y'), ('C', 'x')),
+        (('A', 'x'), ('B', 'y'), ('C', 'y')),
+        (('A', 'y'), ('B', 'x'), ('C', 'y'))
+        ]
+B_IS_SAME_AS_A = {
+        ('x', 'x', 'x', 0): True,
+        ('x', 'x', 'x', 1): False,
+        ('x', 'y', 'y', 0): False,
+        ('x', 'y', 'y', 1): True,
+        ('y', 'x', 'y', 0): False,
+        ('y', 'x', 'y', 1): True,
+        ('y', 'y', 'x', 0): False,
+        ('y', 'y', 'x', 1): True,
+        }
 
 # m = np.array([[1, -1j],[1, 1j]])/np.sqrt(2)
 # m_inv = np.linalg.inv(m)
--- hillery1.py
+++ hillery1.py
@@ -14,17 +14,24 @@ from helpers import ghz, measure_X, measure_Y, assign
 import numpy as np
 from consts import *
 
-N = 3
 sim = WavefunctionSimulator()
-qc = get_qc("5q-qvm")
 
 class Hillery1Party(Party):
+    """ An honest party in the first secret
+    sharing protocol.
+    """
     def __init__(self, name):
-        Party.__init__(self, name)
+        super().__init__(name)
         self.qubit = QubitPlaceholder()
+        self.measurements = []
+        self.directions = []
         self.key = []
 
     def interact(self, secret_share):
+        """Interacts with the HillerySecretShare1
+        object. In particular, it randomly samples a
+        direction in which to measure and announces it
+        """
         self.direction = num2direction[np.random.choice(2)]
         # # TEST A, B measure 'y'
         # self.direction = 'x' if self.name == 'C' else 'y'
@@ -37,49 +44,92 @@ class Hillery1Party(Party):
             secret_share.program += measure_Y(self.qubit, self.ro)
         secret_share.measure_list.append((self.name, self.direction))
 
+# TODO Eavesdropper
+
 class HillerySecretShare1(SecretShare):
     """The first secret sharing protocol from
     Hillery, 1999"""
     def __init__(self, parties):
         super().__init__(parties)
+        self.A, self.B, self.C = self.parties
         self.qubits = [p.qubit for p in parties]
-        self.measure_list = []
         self.init_program()
+        self.qc = get_qc("3q-qvm")
 
     def init_program(self):
+        """Initializes the pyquil program with the ghz
+        state. Also resets the measure_list.
+        """
+        self.measure_list = []
         pq = ghz(self.qubits)
         ro = pq.declare('ro', memory_size=3)
         assign(ro, self.parties, 'ro')
         self.program = pq
 
     def check_valid(self):
-        return True #TODO
+        """Returns true if the the most recent measure_list
+        is a valid one (one in which the honest parties
+        can deduce the secret)
+        """
+        return tuple(self.measure_list) in VALID_DIRECTIONS
 
     def run(self):
+        """ Measures the qubits after each
+        party has announced their measurement direction.
+        """
         if self.check_valid():
             pq = address_qubits(self.program)
-            qc = get_qc("5q-qvm")
-            result = qc.run(pq)
+            result = self.qc.run(pq).flatten()
+            for i, p in enumerate(self.parties):
+                p.measurements.append(result[i])
+                p.directions.append(self.measure_list)
         else:
+            for p in self.parties:
+                p.directions.append('fail')
+                p.measurements.append('fail')
             result = None
         self.init_program()
         return result
 
-    def recombine(self, shares):
-        pass
+def recombine(B, C):
+    key = []
+    for bit_ind, d in enumerate(B.directions):
+        if d != 'fail':
+            indexer = tuple([x[1] for x in d] + [C.measurements[bit_ind]])
+            if B_IS_SAME_AS_A[indexer]:
+                key.append(B.measurements[bit_ind])
+            else:
+                key.append(int(not(B.measurements[bit_ind])))
 
-def test_hillery1():
-    parties = [Hillery1Party(x) for x in 'ABC']
-    qc = get_qc("5q-qvm")
-    ss = HillerySecretShare1(parties)
-    A, B, C = parties
-    A.interact(ss)
-    B.interact(ss)
-    C.interact(ss)
-    return ss.run()
+    B.key = C.key = key
+
+def make_key(A, B, C, key_len=10):
+    """
+    Repeatedly runs the protocol
+    to generate a secret for A such that
+    B and C must cooperate to determine
+    the secret
+    """
+    ss = HillerySecretShare1([A, B, C])
+    n_valid = 0
+    while n_valid < key_len:
+        A.interact(ss)
+        B.interact(ss)
+        C.interact(ss)
+        res = ss.run()
+        if res is not None:
+            n_valid += 1
+
+    A.key = [x for x in A.measurements if x != 'fail']
+    recombine(B, C)
+
 
 if __name__ == '__main__':
-    print(test_hillery1())
+    A, B, C = [Hillery1Party(x) for x in 'ABC']
+    make_key(A, B, C, 20)
+    print(A.key)
+    print(B.key)
+    print(C.key)
 
 
 
